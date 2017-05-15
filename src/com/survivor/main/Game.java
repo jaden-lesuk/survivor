ecac3eeae6e19b63b88bac6a2663f60ac208113d
jaden-lesuk <jadenlesuk12@gmail.com>
2017-05-15 15:47:48 +0300

    Added game loop

--- src/com/survivor/main/Game.java
+++ src/com/survivor/main/Game.java
@@ -7,6 +7,8 @@ public class Game extends Canvas implements Runnable{
 	private static final long serialVersionUID = 8340867905139409543L;
 	
 	public static final int WIDTH = 640, HEIGHT = WIDTH/12 * 9;
+	private Thread thread;
+	private boolean running = false;
 	
 	
 	public Game(){
@@ -14,10 +16,48 @@ public class Game extends Canvas implements Runnable{
 	}
 
 	public synchronized void start(){
+		thread = new Thread(this);
+		thread.start();
+		running = true;
+	}
+	
+	public synchronized void stop(){
+		try{
+			thread.join();
+			running = false;
+			
+		}catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	public void run(){
-		
+		this.requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1){
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				//System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
 	}
 	
 	public static void main(String[] args){
