9ba779047c9d2c66c95a956c9dafb738f41a5236
bryonyoni <bryonyoni@gmail.com>
2017-05-16 11:36:45 +0300

    Added movement to players
    Added methods that make player1 and 2 to move in the game.

--- src/com/survivor/main/KeyInput.java
+++ src/com/survivor/main/KeyInput.java
@@ -19,15 +19,22 @@ public class KeyInput extends KeyAdapter{
 			if(tempObject.getID() == ID.Player){
 				//Key events for player1.
 				
-				if(key == KeyEvent.VK_W) tempObject.setY(tempObject.getY() - 5);
-				if(key == KeyEvent.VK_S) tempObject.setY(tempObject.getY() + 5);
+				if(key == KeyEvent.VK_W) tempObject.setVelY(- 5);
+				if(key == KeyEvent.VK_S) tempObject.setVelY(5);
+				if(key == KeyEvent.VK_A) tempObject.setVelX(- 5);
+				if(key == KeyEvent.VK_D) tempObject.setVelX(5);
+				
 			}
 			
 			if(tempObject.getID() == ID.Player2){
 				//Key events for player1.
 				
-				if(key == KeyEvent.VK_UP) tempObject.setY(tempObject.getY() - 5);
-				if(key == KeyEvent.VK_DOWN) tempObject.setY(tempObject.getY() + 5);
+				if(key == KeyEvent.VK_UP) tempObject.setVelY(- 5);
+				if(key == KeyEvent.VK_DOWN) tempObject.setVelY(5);
+				if(key == KeyEvent.VK_LEFT) tempObject.setVelX(- 5);
+				if(key == KeyEvent.VK_RIGHT) tempObject.setVelX(5);
+				
+				
 
 			}
 		}
@@ -35,6 +42,31 @@ public class KeyInput extends KeyAdapter{
 	
 	public void keyReleased(KeyEvent e){
 		int key = e.getKeyCode();
+		
+		for(int i = 0; i < handler.object.size() ; i++){
+			GameObject tempObject = handler.object.get(i);
+			
+			if(tempObject.getID() == ID.Player){
+				//Key events for player1.
+				
+				if(key == KeyEvent.VK_W) tempObject.setVelY(0);
+				if(key == KeyEvent.VK_S) tempObject.setVelY(0);
+				if(key == KeyEvent.VK_A) tempObject.setVelX(0);
+				if(key == KeyEvent.VK_D) tempObject.setVelX(0);
+				
+			}
+			
+			if(tempObject.getID() == ID.Player2){
+				//Key events for player1.
+				
+				if(key == KeyEvent.VK_UP) tempObject.setVelY(0);
+				if(key == KeyEvent.VK_DOWN) tempObject.setVelY(0);
+				if(key == KeyEvent.VK_LEFT) tempObject.setVelX(0);
+				if(key == KeyEvent.VK_RIGHT) tempObject.setVelX(0);
+				
+
+			}
+		}
 	}
 	
 }
