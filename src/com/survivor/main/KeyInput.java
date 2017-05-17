7d383b0c0727726400e7d12a2df3da05553638e4
bryonyoni <bryonyoni@gmail.com>
2017-05-17 09:23:46 +0300

    Fixed bug in player movement

--- src/com/survivor/main/KeyInput.java
+++ src/com/survivor/main/KeyInput.java
@@ -5,9 +5,14 @@ import java.awt.event.KeyEvent;
 public class KeyInput extends KeyAdapter{
 	
 	private Handler handler;
+	private boolean[] keyDown = new boolean[4];
 	
 	public KeyInput(Handler handler){
 		this.handler = handler;
+		keyDown[0] = false; //w key
+		keyDown[1] = false; //s key
+		keyDown[2] = false; //d key		
+		keyDown[3] = false; //a key
 	}
 	
 	public void keyPressed(KeyEvent e){
@@ -19,11 +24,10 @@ public class KeyInput extends KeyAdapter{
 			if(tempObject.getID() == ID.Player){
 				//Key events for player1.
 				
-				if(key == KeyEvent.VK_W) tempObject.setVelY(- 5);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(5);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(- 5);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(5);
-				
+				if(key == KeyEvent.VK_W) { tempObject.setVelY(- 5); keyDown[0] = true;  }
+				if(key == KeyEvent.VK_S) { tempObject.setVelY(5);   keyDown[1] = true;  }
+				if(key == KeyEvent.VK_D) { tempObject.setVelX(5);   keyDown[2] = true;  }
+				if(key == KeyEvent.VK_A) { tempObject.setVelX(- 5); keyDown[3] = true;  }				
 			}
 			
 		}
@@ -40,11 +44,17 @@ public class KeyInput extends KeyAdapter{
 			if(tempObject.getID() == ID.Player){
 				//Key events for player1.
 				
-				if(key == KeyEvent.VK_W) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_S) tempObject.setVelY(0);
-				if(key == KeyEvent.VK_A) tempObject.setVelX(0);
-				if(key == KeyEvent.VK_D) tempObject.setVelX(0);
+				if(key == KeyEvent.VK_W) keyDown[0] = false ;
+				if(key == KeyEvent.VK_S) keyDown[1] = false ;
+				if(key == KeyEvent.VK_A) keyDown[3] = false ;
+				if(key == KeyEvent.VK_D) keyDown[2] = false ;
 				
+				//vertical
+				if(!keyDown[0] && !keyDown[1])tempObject.setVelY(0);
+				
+				//horizontal
+				if(!keyDown[2] && !keyDown[3])tempObject.setVelX(0);
+
 			}
 		}
 	}
